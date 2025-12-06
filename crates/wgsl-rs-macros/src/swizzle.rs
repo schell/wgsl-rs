83cb65e0e53cf1a4ba7b2ad0134c59e8edd68667
Schell Carl Scivally <efsubenovex@gmail.com>
2025-12-07 09:29:41 +1300

    feat: enhance swizzle macro to support additional fields and swizzles

--- crates/wgsl-rs-macros/src/swizzle.rs
+++ crates/wgsl-rs-macros/src/swizzle.rs
@@ -1,6 +1,7 @@
 //! Swizzle implementation macro.
 
 use proc_macro::TokenStream;
+use quote::ToTokens;
 use syn::parse::Parse;
 
 /// Parses macro input like `Vec2, [x, y], [r, g]` and
@@ -9,6 +10,8 @@ use syn::parse::Parse;
 /// `r` and `g` point to `x` and `y`.
 struct Swizzling {
     ty: syn::Ident,
+    fields: Vec<syn::Ident>,
+    swizzles: Vec<syn::Ident>,
 }
 
 impl Parse for Swizzling {
@@ -16,29 +19,37 @@ impl Parse for Swizzling {
         let ty = syn::Ident::parse(input)?;
         let _comma = <syn::Token![,]>::parse(input)?;
 
-        // Parse the first bracketed term: [x, y]
+        // Parse the first bracketed term, eg [x, y, z, w]
         let bracketed1;
         syn::bracketed!(bracketed1 in input);
-        let _idents1: syn::punctuated::Punctuated<syn::Ident, syn::Token![,]> =
-            bracketed1.parse_terminated(syn::Ident::parse)?;
-
-        // Optionally parse a comma and second bracketed term: [r, g]
-        let _idents2 = if input.peek(syn::Token![,]) {
+        // Parse the fields to access when swizzling
+        let fields = bracketed1.parse_terminated(syn::Ident::parse, syn::Token![,])?;
+        // Optionally parse the identifiers for the swizzle function, eg [r, g, b, a]
+        // If these are omitted we'll use the accessors
+        let swizzles = if input.peek(syn::Token![,]) {
             let _comma2: syn::Token![,] = input.parse()?;
             let bracketed2;
             syn::bracketed!(bracketed2 in input);
-            Some(
-                bracketed2.parse_terminated(syn::Ident::parse)?
-            )
+            bracketed2.parse_terminated(syn::Ident::parse, syn::Token![,])?
         } else {
-            None
+            fields.clone()
         };
 
-        Err(syn::Error::new(input.span(), format!("{input:#?}")))
+        Ok(Self {
+            ty,
+            fields: fields.into_iter().collect(),
+            swizzles: swizzles.into_iter().collect(),
+        })
+    }
+}
+
+impl ToTokens for Swizzling {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        todo!()
     }
 }
 
 pub fn swizzle(token_stream: TokenStream) -> TokenStream {
     let swizzling: Swizzling = syn::parse_macro_input!(token_stream);
-    quote::quote! {}.into()
+    swizzling.into_token_stream().into()
 }

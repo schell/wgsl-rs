46c03a3d15dac36811555296016ca183f6e7e5cd
Schell Carl Scivally <efsubenovex@gmail.com>
2025-12-07 09:21:38 +1300

    feat: add swizzle macros for WGSL in wgsl-rs-macros crate

---
+++ crates/wgsl-rs-macros/src/swizzle.rs
@@ -0,0 +1,27 @@
+//! Swizzle implementation macro.
+
+use proc_macro::TokenStream;
+use syn::parse::Parse;
+
+/// Parses macro input like `Vec2, [x, y], [r, g]` and
+/// produces swizzle function implementations of for
+/// functions x, y, r, g, xx, xy, yy, yx, rr, rg, gg, gr.
+/// `r` and `g` point to `x` and `y`.
+struct Swizzling {
+    ty: syn::Ident,
+}
+
+impl Parse for Swizzling {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ty = syn::Ident::parse(input)?;
+        let _comma = <syn::Token![,]>::parse(input)?;
+        let _bracket = todo!();
+
+        Err(syn::Error::new(input.span(), format!("{input:#?}")))
+    }
+}
+
+pub fn swizzle(token_stream: TokenStream) -> TokenStream {
+    let swizzling: Swizzling = syn::parse_macro_input!(token_stream);
+    quote::quote! {}.into()
+}

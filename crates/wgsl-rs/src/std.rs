897863aed178e851264ac1d763d5381844debe93
Schell Carl Scivally <efsubenovex@gmail.com>
2025-12-08 08:18:18 +1300

    swizzling

--- crates/wgsl-rs/src/std.rs
+++ crates/wgsl-rs/src/std.rs
@@ -19,64 +19,178 @@ pub trait IsScalar: Sized {
     fn vec4(x: Self, y: Self, z: Self, w: Self) -> Vec4<Self>;
 }
 
-// #[repr(transparent)]
-// pub struct Vec2<T: IsScalar> {
-//     inner: T::Vector2,
-// }
-// pub type Vec2f = Vec2<f32>;
-// pub const fn vec2f(x: f32, y: f32) -> Vec2f {
-//     Vec2 {
-//         inner: glam::Vec2::new(x, y),
-//     }
-// }
-// wgsl_rs_macros::swizzle!(Vec2f, [f32, Vec2f], [vec2f], [x, y], [r, g]);
-// wgsl_rs_macros::swizzle!(Vec2f, [f32, Vec2f], [vec2f], [x, y], [x, y]);
-
+/// vector! generates the N-vector type and constructors
 macro_rules! vector {
-    // Example: vector!(2, f, f32, [x, y], [r, g]);
-    ($n:literal, $t:ident, $ty:ty, [$($field:ident),+], [$($swizzle:ident),+]) => {
+    // Example: vector!(2, [x, y]);
+    ($n:literal, [$($field:ident),+]) => {
         paste::paste! {
-            /// A vector in N dimensions.
+            /// A vector.
             #[repr(transparent)]
+            #[derive(Clone, Copy)]
             pub struct [<Vec $n>]<T: IsScalar> {
                 inner: T::[<Vector $n>],
             }
-            pub type [<Vec $n $t>] = [<Vec $n>]<$ty>;
-            /// Vector constructor
-            pub const [<vec $n $t>]($($field: $ty),+) -> [<Vec $n $t>] {
-                [<Vec $n>] {
-                    inner: glam::[<Vec $n>]::new($($field),+),
-                }
+
+            /// Alias for `Vec{N}::vec{n}`, since that syntax cannot produce valid WGSL.
+            pub fn [<vec $n>]<T: IsScalar>($($field: T),+) -> [<Vec$n>]<T> {
+                T::[<vec $n>]($($field),+)
             }
-            wgsl_rs_macros::swizzle!([<Vec $n $t>], [$ty, [<Vec $n $t>]], [[<vec $n $t>]], [$($field),+], [$($swizzle),+]);
-            wgsl_rs_macros::swizzle!([<Vec $n $t>], [$ty, [<Vec $n $t>]], [[<vec $n $t>]], [$($field),+], [$($field),+]);
         }
     }
 }
 
-vector!(2, f, f32, [x, y], [r, g]);
-
-#[repr(transparent)]
-pub struct Vec3<T: IsScalar> {
-    inner: T::Vector3,
-}
-pub type Vec3f = Vec3<f32>;
-pub const fn vec3f(x: f32, y: f32, z: f32) -> Vec3f {
-    Vec3 {
-        inner: glam::Vec3::new(x, y, z),
+/// aliases! generates aliases for the different component types.
+macro_rules! aliases {
+    ($n: literal, $t:ident, $ty:ty, $glam_ty:ty, [$($field:ident),+]) => {
+        paste::paste! {
+            pub type [<Vec $n $t>] = [<Vec $n>]<$ty>;
+            /// Vector constructor
+            pub const fn [<vec $n $t>]($($field: $ty),+) -> [<Vec $n $t>] {
+                [<Vec $n>] {
+                    inner: glam::[<$glam_ty $n>]::new($($field),+),
+                }
+            }
+        }
     }
 }
 
-#[repr(transparent)]
-pub struct Vec4<T: IsScalar> {
-    inner: T::Vector4,
-}
-pub type Vec4f = Vec4<f32>;
-pub const fn vec4f(x: f32, y: f32, z: f32, w: f32) -> Vec4f {
-    Vec4 {
-        inner: glam::Vec4::new(x, y, z, w),
-    }
-}
+vector!(2, [x, y]);
+aliases!(2, f, f32, Vec, [x, y]);
+aliases!(2, i, i32, IVec, [x, y]);
+aliases!(2, u, u32, UVec, [x, y]);
+aliases!(2, b, bool, BVec, [x, y]);
+wgsl_rs_macros::swizzle!(Vec2f, [f32, Vec2f], [vec2f], [x, y], [r, g]);
+wgsl_rs_macros::swizzle!(Vec2f, [f32, Vec2f], [vec2f], [x, y], [x, y]);
+wgsl_rs_macros::swizzle!(Vec2i, [i32, Vec2i], [vec2i], [x, y], [r, g]);
+wgsl_rs_macros::swizzle!(Vec2i, [i32, Vec2i], [vec2i], [x, y], [x, y]);
+wgsl_rs_macros::swizzle!(Vec2u, [u32, Vec2u], [vec2u], [x, y], [r, g]);
+wgsl_rs_macros::swizzle!(Vec2u, [u32, Vec2u], [vec2u], [x, y], [x, y]);
+wgsl_rs_macros::swizzle!(Vec2b, [bool, Vec2b], [vec2b], [x, y], [r, g]);
+wgsl_rs_macros::swizzle!(Vec2b, [bool, Vec2b], [vec2b], [x, y], [x, y]);
+
+vector!(3, [x, y, z]);
+aliases!(3, f, f32, Vec, [x, y, z]);
+aliases!(3, i, i32, IVec, [x, y, z]);
+aliases!(3, u, u32, UVec, [x, y, z]);
+aliases!(3, b, bool, BVec, [x, y, z]);
+wgsl_rs_macros::swizzle!(
+    Vec3f,
+    [f32, Vec2f, Vec3f],
+    [vec2f, vec3f],
+    [x, y, z],
+    [r, g, b]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3f,
+    [f32, Vec2f, Vec3f],
+    [vec2f, vec3f],
+    [x, y, z],
+    [x, y, z]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3i,
+    [i32, Vec2i, Vec3i],
+    [vec2i, vec3i],
+    [x, y, z],
+    [r, g, b]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3i,
+    [i32, Vec2i, Vec3i],
+    [vec2i, vec3i],
+    [x, y, z],
+    [x, y, z]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3u,
+    [u32, Vec2u, Vec3u],
+    [vec2u, vec3u],
+    [x, y, z],
+    [r, g, b]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3u,
+    [u32, Vec2u, Vec3u],
+    [vec2u, vec3u],
+    [x, y, z],
+    [x, y, z]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3b,
+    [bool, Vec2b, Vec3b],
+    [vec2b, vec3b],
+    [x, y, z],
+    [r, g, b]
+);
+wgsl_rs_macros::swizzle!(
+    Vec3b,
+    [bool, Vec2b, Vec3b],
+    [vec2b, vec3b],
+    [x, y, z],
+    [x, y, z]
+);
+
+vector!(4, [x, y, z, w]);
+aliases!(4, f, f32, Vec, [x, y, z, w]);
+aliases!(4, i, i32, IVec, [x, y, z, w]);
+aliases!(4, u, u32, UVec, [x, y, z, w]);
+aliases!(4, b, bool, BVec, [x, y, z, w]);
+wgsl_rs_macros::swizzle!(
+    Vec4f,
+    [f32, Vec2f, Vec3f, Vec4f],
+    [vec2f, vec3f, vec4f],
+    [x, y, z, w],
+    [r, g, b, a]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4f,
+    [f32, Vec2f, Vec3f, Vec4f],
+    [vec2f, vec3f, vec4f],
+    [x, y, z, w],
+    [x, y, z, w]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4i,
+    [i32, Vec2i, Vec3i, Vec4i],
+    [vec2i, vec3i, vec4i],
+    [x, y, z, w],
+    [r, g, b, a]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4i,
+    [i32, Vec2i, Vec3i, Vec4i],
+    [vec2i, vec3i, vec4i],
+    [x, y, z, w],
+    [x, y, z, w]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4u,
+    [u32, Vec2u, Vec3u, Vec4u],
+    [vec2u, vec3u, vec4u],
+    [x, y, z, w],
+    [r, g, b, a]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4u,
+    [u32, Vec2u, Vec3u, Vec4u],
+    [vec2u, vec3u, vec4u],
+    [x, y, z, w],
+    [x, y, z, w]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4b,
+    [bool, Vec2b, Vec3b, Vec4b],
+    [vec2b, vec3b, vec4b],
+    [x, y, z, w],
+    [r, g, b, a]
+);
+wgsl_rs_macros::swizzle!(
+    Vec4b,
+    [bool, Vec2b, Vec3b, Vec4b],
+    [vec2b, vec3b, vec4b],
+    [x, y, z, w],
+    [x, y, z, w]
+);
 
 macro_rules! impl_is_scalar {
     ($ty:ty, $vec2:ty, $vec3:ty, $vec4:ty) => {
@@ -109,7 +223,3 @@ impl_is_scalar!(f32, glam::Vec2, glam::Vec3, glam::Vec4);
 impl_is_scalar!(i32, glam::IVec2, glam::IVec3, glam::IVec4);
 impl_is_scalar!(u32, glam::UVec2, glam::UVec3, glam::UVec4);
 impl_is_scalar!(bool, glam::BVec2, glam::BVec3, glam::BVec4);
-
-pub fn vec4<T: IsScalar>(x: T, y: T, z: T, w: T) -> Vec4<T> {
-    T::vec4(x, y, z, w)
-}
